4d3a12fce62c2793dbcbd980a594619a45438ceb
happyclaw-agent <happytreeiot@gmail.com>
2026-02-08 21:27:01 +0000

    cleanup: remove unnecessary files (fix_syntax.py, show_line.py, HACKATHON_PLAN.md, TWEETS_SECURITY_THREAD.md)

--- show_line.py
+++
@@ -1,5 +0,0 @@
-with open('src/trustyclaw/sdk/escrow_contract.py') as f:
-    lines = f.readlines()
-print('Line 657:', repr(lines[656]))
-print('Line 658:', repr(lines[657]))
-print('Line 659:', repr(lines[658]))
